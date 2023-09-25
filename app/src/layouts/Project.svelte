850772ca43a206df7f075d564ee3cd29b18a672f
Filip Ljubic <filip.ljubic@fer.hr>
2023-09-25 19:20:26 +0200

    project layout

---
+++ app/src/layouts/Project.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+	import type { Project } from '$lib/utils/types/sanityTypes';
+	import PortableTextExtended from '../components/PortableTextExtended.svelte';
+
+	export let project: Project;
+</script>
+
+<section class="pt-32 px-6 md:px-14">
+	<div class="flex flex-col gap-6">
+		<p>
+			{formatDate(project._createdAt)}
+		</p>
+
+		<h1 class="text-5xl md:text-7xl">{project.title}</h1>
+
+		<div>
+			<PortableTextExtended value={project.body} />
+		</div>
+	</div>
+</section>
--- app/src/routes/projekti/[slug]/+page.svelte
+++ app/src/routes/projekti/[slug]/+page.svelte
@@ -1,29 +1,8 @@
 <script lang="ts">
-	import { formatDate } from '$lib/utils';
-	import { urlFor } from '$lib/utils/image';
-	import PortableTextExtended from '../../../components/PortableTextExtended.svelte';
+	import Project from '../../../layouts/Project.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<section>
-	{#if data.mainImage}
-		<picture>
-			<source media="(max-width: 1024px)" srcSet={urlFor(data.mainImage).maxWidth(400).url()} />
-			<img src={urlFor(data.mainImage).width(1200).url()} alt={`Cover image for ${data.title}`} />
-		</picture>
-	{:else}
-		<div />
-	{/if}
-	<div>
-		<h1>{data.title}</h1>
-		<p>{data.excerpt}</p>
-		<p>
-			{formatDate(data._createdAt)}
-		</p>
-		<div>
-			<PortableTextExtended value={data.body} />
-		</div>
-	</div>
-</section>
+<Project project={data} />
